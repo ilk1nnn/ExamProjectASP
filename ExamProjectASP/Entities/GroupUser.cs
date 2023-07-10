38353411f5525eb128ab258074870c8e4df0f174
Huseyn Abbasov <nelbekistekan12@gmail.com>
2023-07-10 22:29:26 +0400

    Done

--- ExamProjectASP/Entities/GroupUser.cs
+++ ExamProjectASP/Entities/GroupUser.cs
@@ -2,5 +2,11 @@
 {
 	public class GroupUser
 	{
+		public int Id { get; set; }
+		public int UserId { get; set; }
+		public int GroupId { get; set; }
+		public CustomIdentityUser User { get; set; }
+		public Group Group { get; set; }
 	}
+
 }
